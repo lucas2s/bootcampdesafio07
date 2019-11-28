1f49d3a6e0e93b36aef0c60a7dd57482b729c5ed
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2019-11-28 01:10:49 -0300

    Criado a index e styles Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,8 +1,71 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { Component } from 'react';
+import { ActivityIndicator } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import {
+  Container,
+  ListProduct,
+  Product,
+  ProductImage,
+  ProductTitle,
+  ProductPrice,
+  AddCart,
+  NameButton,
+  ProductAdd,
+  ProductAmount,
+  Amount,
+} from './styles';
+// import { formatPrice } from '../../util/format';
+import api from '../../services/api';
 
-// import { Container } from './styles';
+export default class Main extends Component {
+  state = {
+    products: [],
+    loading: false,
+    amount: [],
+  };
 
-export default function Main() {
-  return <View />;
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      // priceFormatted: formatPrice(product.price),
+      priceFormatted: product.price,
+    }));
+
+    this.setState({ products: data });
+  }
+
+  render() {
+    const { products, loading, amount } = this.state;
+    return (
+      <Container>
+        <ListProduct
+          horizontal
+          data={products}
+          keyExtractor={product => String(product.id)}
+          renderItem={({ item }) => (
+            <Product>
+              <ProductImage source={{ uri: item.image }} />
+              <ProductTitle>{item.title}</ProductTitle>
+              <ProductPrice>{item.priceFormatted}</ProductPrice>
+              <AddCart loading={loading} onPress={this.handleAddUser}>
+                <ProductAmount>
+                  <Icon name="add-shopping-cart" color="#FFF" size={16} />
+                  <Amount> {amount[item.id] || 0} </Amount>
+                </ProductAmount>
+                <ProductAdd loading={loading} onPress={this.handleAddUser}>
+                  {loading ? (
+                    <ActivityIndicator color="#fff" />
+                  ) : (
+                    <NameButton>ADICIONAR</NameButton>
+                  )}
+                </ProductAdd>
+              </AddCart>
+            </Product>
+          )}
+        />
+      </Container>
+    );
+  }
 }
