a495ead62d46eabd1008769a2918027b8a74410a
Lucas Silva Sartori <sartori.s.lucas@gmail.com>
2019-11-30 02:24:10 -0300

    Inicio da criação e estilização do carrinho

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,89 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
-import { Container } from './styles';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
-export default function Cart() {
-  return <Container />;
+import * as CartActions from '../../store/modules/cart/actions';
+
+import { formatPrice } from '../../util/format';
+
+import {
+  Container,
+  ListCartProduct,
+  Product,
+  ProductDetails,
+  ProductDesc,
+  ProductImage,
+  ProductTitle,
+  ProductPrice,
+  DeleteProduct,
+  ProductStatus,
+  DecrementProduct,
+  IncrementProduct,
+  ProductAmount,
+  ProductSubtotal,
+  ProductUpddateAmount,
+} from './styles';
+
+function Cart({ cart, total, removeFromCart, updateAmountRequest }) {
+  function increment(product) {
+    updateAmountRequest(product.id, product.amount + 1);
+  }
+
+  function decrement(product) {
+    updateAmountRequest(product.id, product.amount - 1);
+  }
+
+  return (
+    <Container>
+      <ListCartProduct
+        data={cart}
+        keyExtractor={product => String(product.id)}
+        renderItem={({ item }) => (
+          <Product>
+            <ProductDetails>
+              <ProductImage source={{ uri: item.image }} />
+              <ProductDesc>
+                <ProductTitle>{item.title}</ProductTitle>
+                <ProductPrice>{item.priceFormatted}</ProductPrice>
+              </ProductDesc>
+              <DeleteProduct onPress={() => removeFromCart(item.id)}>
+                <Icon name="delete" color="#7159C1" size={24} />
+              </DeleteProduct>
+            </ProductDetails>
+            <ProductStatus>
+              <ProductUpddateAmount>
+                <DecrementProduct onPress={() => decrement(item)}>
+                  <Icon name="remove" color="#7159C1" size={24} />
+                </DecrementProduct>
+                <ProductAmount value={String(item.amount)} />
+                <IncrementProduct onPress={() => increment(item)}>
+                  <Icon name="add" color="#7159C1" size={24} />
+                </IncrementProduct>
+              </ProductUpddateAmount>
+              <ProductSubtotal>{item.subtotal}</ProductSubtotal>
+            </ProductStatus>
+          </Product>
+        )}
+      />
+    </Container>
+  );
 }
+
+const mapStateToProps = state => ({
+  cart: state.cart.map(product => ({
+    ...product,
+    subtotal: formatPrice(product.price * product.amount),
+  })),
+  total: formatPrice(
+    state.cart.reduce((total, product) => {
+      return total + product.price * product.amount;
+    }, 0)
+  ),
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(CartActions, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(Cart);
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -4,3 +4,93 @@ export const Container = styled.View`
   flex: 1;
   background: #191920;
 `;
+
+export const ListCartProduct = styled.FlatList.attrs({
+  showsHorizontalScrollIndicator: false,
+})`
+  margin: 20px;
+  background: #fff;
+  border-radius: 4px;
+  flex-grow: 0;
+`;
+
+export const Product = styled.View`
+  margin: 20px;
+`;
+
+export const ProductDetails = styled.View`
+  flex-direction: row;
+  align-items: center;
+`;
+
+export const ProductDesc = styled.View`
+  max-width: 163;
+`;
+
+export const ProductImage = styled.Image`
+  height: 80px;
+  width: 80px;
+  margin-right: 10px;
+`;
+
+export const ProductTitle = styled.Text.attrs({
+  numberOfLines: 2,
+})`
+  font-size: 14px;
+  line-height: 18px;
+  text-align: left;
+  font-family: Roboto;
+`;
+export const ProductPrice = styled.Text`
+  font-size: 16px;
+  text-align: left;
+  font-weight: bold;
+  font-family: Roboto;
+`;
+
+export const DeleteProduct = styled.TouchableOpacity`
+  padding: 6px;
+  margin-left: auto;
+`;
+
+export const ProductStatus = styled.View`
+  flex: 1;
+  flex-direction: row;
+  background: #eee;
+  height: 40px;
+  justify-content: space-between;
+  align-items: center;
+  border: 1px solid #dddddd;
+  border-radius: 4px;
+  padding: 5px;
+`;
+
+export const DecrementProduct = styled.TouchableOpacity`
+  padding: 6px;
+`;
+
+export const IncrementProduct = styled.TouchableOpacity`
+  padding: 6px;
+`;
+
+export const ProductAmount = styled.TextInput.attrs({
+  readonly: true,
+})`
+  background: #fff;
+  padding: 5px;
+  margin: 0 5px;
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  min-width: 52px;
+  text-align: center;
+`;
+
+export const ProductSubtotal = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+  font-family: Roboto;
+`;
+
+export const ProductUpddateAmount = styled.View`
+  flex-direction: row;
+`;
